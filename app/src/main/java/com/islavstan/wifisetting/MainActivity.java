b1d235c03da1c8252797cc3edf3abe75b3fe352b
islavstan <islavstan@gmail.com>
2017-03-15 17:45:00 +0300

    check internet

--- app/src/main/java/com/islavstan/wifisetting/MainActivity.java
+++ app/src/main/java/com/islavstan/wifisetting/MainActivity.java
@@ -1,25 +1,30 @@
 package com.islavstan.wifisetting;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import cc.mvdan.accesspoint.WifiApControl;
 
 public class MainActivity extends AppCompatActivity {
-    String SSIDname = "VOMER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+//http://stackoverflow.com/questions/17152847/create-wifi-hotspot-configuration-in-android
+//http://stackoverflow.com/questions/32817552/how-to-enable-mobile-data-on-off-programmatically
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
 
@@ -29,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         WifiConfiguration netConfig = new WifiConfiguration();
 
-        netConfig.SSID = "MyAP";
+        netConfig.SSID = "VOMER";
         netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
         netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
         netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
@@ -47,14 +52,29 @@ public class MainActivity extends AppCompatActivity {
             int apstate = (Integer) getWifiApStateMethod.invoke(wifiManager);
             Method getWifiApConfigurationMethod = wifiManager.getClass().getMethod("getWifiApConfiguration");
             netConfig = (WifiConfiguration) getWifiApConfigurationMethod.invoke(wifiManager);
-            Log.e("CLIENT", "\nSSID:" + netConfig.SSID + "\nPassword:" + netConfig.preSharedKey + "\n");
+            Log.d("stas", "\nSSID:" + netConfig.SSID + "\nPassword:" + netConfig.preSharedKey + "\n");
 
         } catch (Exception e) {
             Log.e(this.getClass().toString(), "", e);
         }
 
+        WifiApControl apControl = WifiApControl.getInstance(this);
 
+        apControl.enable();
+
+        Log.d("stas", isMobileConnected(this) + " internet ");
+    }
+
+
+    public static boolean isMobileConnected(Context context) {
+        ConnectivityManager connManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        return ((netInfo != null) && netInfo.isConnected());
     }
 
 
 }
+
+
+
